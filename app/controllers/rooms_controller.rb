b1718f1046f394a139b97cf36a29d807fa94968b
sakitayu <sakitakitakita@gmail.com>
2020-06-26 01:12:53 +0900

    メッセージ削除機能を実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,10 @@ class RoomsController < ApplicationController
   def show
     @messages = Message.all
   end
+
+  def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
+    redirect_to room_path
+  end
 end
