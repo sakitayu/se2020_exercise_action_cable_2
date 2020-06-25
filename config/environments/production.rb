f843564f6d7d1c28429f58bad4d6e7aa5187ef15
sakitayu <sakitakitakita@gmail.com>
2020-06-25 23:37:58 +0900

    production.rbとcable.ymlを編集

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,9 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  #Action Cableが動作するらしいンゴ...
+  config.action_cable.allowed_request_origins = [ /http:\/\/.*/ ]
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
